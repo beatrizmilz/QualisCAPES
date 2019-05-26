eece97a7a3575326eadf1e9194623af37aa3b0b4
beatrizmilz <beatriz.milz@hotmail.com>
2019-05-26 17:21:55 -0300

    identifica o erro que está causando o sumiço de dados

--- app.R
+++ app.R
@@ -79,8 +79,8 @@ server <- function(input, output) {
   
   filtered_data <- reactive({
    qualis_capes <- qualis_capes %>%
-     filter(estrato %in% input$estrato_i & 
-              area_de_avaliacao %in% input$area_de_avaliacao_i) %>% 
+    #filter(estrato %in% input$estrato_i) %>% # isso ta dando erro no codigo
+     filter(area_de_avaliacao %in% input$area_de_avaliacao_i) %>% 
      arrange(estrato) %>% 
      rename(ISSN = issn,
             `Título` = titulo,
