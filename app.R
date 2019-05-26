eb4ddee2c5f68bda2fcf436315c60a670a0f268c
beatrizmilz <beatriz.milz@hotmail.com>
2019-05-26 15:36:33 -0300

    adiciona a tabela, usa o shinywidgets para alterar as formas de input

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(DT)
+library(shinyWidgets)
 library(shinydashboard)
 
 qualis_capes <- read_rds("output/qualis_capes.rds")
@@ -27,26 +28,29 @@ ui <- dashboardPage(
               h2("Pesquisa de periódicos"),
                fluidRow( #uma linha
                 box(#caixa em uma linha
-                  selectInput(
-                    inputId = "area_de_avaliacao",
+                  pickerInput(
+                    inputId = "area_de_avaliacao_i",
                     label = "Área de Avaliação",
-                    choices =  sort(unique(qualis_capes$area_de_avaliacao)),  
-                    multiple = TRUE
+                    choices =  sort(unique(qualis_capes$area_de_avaliacao)), 
+                    options = list(`actions-box` = TRUE),
+                    multiple = TRUE,
+                    selected = TRUE
                   )), 
                 
                 box(
                   
-                  selectInput(
-                    inputId = "slider_estrato",
+                  sliderTextInput(
+                    inputId = "estrato_i",
                     label = "Escolha a classificação no periódicos CAPES",
                    choices = sort(unique(qualis_capes$estrato)),
-                   multiple = TRUE
+                   selected = c("A1", "C"),
+                   grid = TRUE
                   )
                 ) # caixa em uma linha
               ),
               
               # Adiciona a tabela
-                DTOutput("resultado_pesquisa")
+                DTOutput("tabela_periodicos")
       ),
       
       # Tab do projeto
@@ -67,6 +71,26 @@ ui <- dashboardPage(
 
 
 server <- function(input, output) {
+  
+  filtered_data <- reactive({
+   qualis_capes <- qualis_capes %>%
+     filter(estrato %in% input$estrato_i) %>% 
+     filter(area_de_avaliacao %in% input$area_de_avaliacao_i) %>% 
+     arrange(estrato) %>% 
+     rename(ISSN = issn,
+            `Título` = titulo,
+            `Área de Avaliação` = area_de_avaliacao,
+            Estrato = estrato,
+            `Período de avaliação` = avaliacao) 
+
+    
+   qualis_capes
+  })
+  
+  output$tabela_periodicos <- renderDT({
+    filtered_data() 
+    } , escape = FALSE)
+  
   
 }
 
