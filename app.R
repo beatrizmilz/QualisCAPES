c8da903da704eaff384dadb32a567363558fd4c8
beatrizmilz <beatriz.milz@hotmail.com>
2019-05-26 14:32:48 -0300

    utiliza o shinydashboard para criar a base da página

--- app.R
+++ app.R
@@ -1,12 +1,54 @@
 library(shiny)
+library(shinydashboard)
 
 qualis_capes <- read_rds("output/qualis_capes.rds")
 
-ui <- fluidPage(
+ui <- dashboardPage(
   
+  # Título
+  dashboardHeader(title = "Pesquisa de periódicos", titleWidth = 260),
+  ## Conteúdo da sidebar
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Pesquisa de Periódicos", tabName = "dashboard", icon = icon("search")),
+      menuItem("Sobre o projeto", tabName = "sobre", icon = icon("graduation-cap")),
+      menuItem(" Acesse o código", tabName = "github", icon = icon("github-square"))
+    )
+  ),
+  
+  ## Conteúdo da parte principal
+  ## Body content
+  dashboardBody(
+    tabItems(
+      # Tab do dashboard - pesquisa
+      tabItem(tabName = "dashboard",
+              # Colocar aqui o conteúdo da página da pesquisa
+              h2("Pesquisa de periódicos"),
+               fluidRow( #uma linha
+                box(), #caixa em uma linha
+                
+                box() # caixa em uma linha
+              )
+      ),
+      
+      # Tab do projeto
+      tabItem(tabName = "sobre",
+              # Colocar aqui a parte do projeto
+              h2("Sobre o projeto")
+      ),
+      # Tab do github  
+      tabItem(tabName = "github",
+              # Colocar aqui a parte do projeto
+              h2("Acesse o código")
+      )
+      
+    )
+  )
 )
 
-server <- function(input, output, session) {
+
+
+server <- function(input, output) {
   
 }
 
